430c35015536b0e705663ebabf971a97a93210f0
Abecedariann <1102508091@qq.com>
2018-04-27 23:14:41 +0800

    on_linux

---
+++ WebHub/WebHub/pornhub_type.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+"""归纳PornHub资源链接"""
+PH_TYPES = [
+    '',
+    'recommended',
+    'video?o=ht', # hot
+    'video?o=mv', # Most Viewed
+    'video?o=tr', # Top Rate
+    'video?c=Japanese',
+
+    # Examples of certain categories
+    # 'video?c=1',  # Category = Asian
+    # 'video?c=111',  # Category = Japanese
+]
